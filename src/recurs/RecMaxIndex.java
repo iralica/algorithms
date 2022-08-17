7935887b37d227626c17aa997083807494d09379
comp1 <kiralica01112004@gmail.com>
2022-08-17 15:43:02 +0300

    new 1

--- src/recurs/RecMaxIndex.java
+++ src/recurs/RecMaxIndex.java
@@ -3,14 +3,14 @@ package recurs;
 public class RecMaxIndex {
     public static void main(String[] args) {
         int[] ar = new int[]{2, 4, 8, 10, 12};
-        int[] ar1 = new int[]{12};
 
-        System.out.println();
-    }
-    private static int maxIndex(int[] ar, int firstIndex, int lastIndex) {
+        System.out.println("Max element is: " + arMax(ar, ar.length));
 
-
-        return 0; //?
     }
 
+    public static int arMax(int[] ar, int i) {
+        if (i == 1)
+            return ar[0];
+        return Math.max(ar[i-1], arMax(ar,i-1));
+    }
 }
