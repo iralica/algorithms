0c9541d3490cf7371edfc59a089b8ebb2970c1e8
ira <kiralica01112004@gmail.com>
2022-08-24 13:49:34 +0300

    today 5

--- src/HomeWork3/PosInEndArr.java
+++ src/HomeWork3/PosInEndArr.java
@@ -9,9 +9,36 @@ public class PosInEndArr {
         PosInEndArr posInEndArr = new PosInEndArr();
         int [] m = {100, 112, 256, 349, 770};
         int [] n = {72, 86, 113, 119, 265, 445, 892};
+        long element = posInEndArr.kthElement(m, n, m.length, n.length, 7);
+        System.out.println(element);
     }
+    static long  maxN = (long)1e10;
+    private int upperBound(int[] a, int low, int high, long element){  // ищет если к это не середина
+        while(low < high){
+            int middle = low + (high - low)/2;
+            if(a[middle] > element)
+                high = middle;
+            else
+                low = middle + 1;
+        }
+        return low;
+    }
+    private long  kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        long  left = 1, right = maxN;
+        long  ans = (long)1e15;
 
-    /*private int Position (int [] m, int [] n){
+        while (left <= right) {
+            long  mid = (left + right) / 2;
+            long  up_cnt = upperBound(arr1,0, n, mid);
+            up_cnt += upperBound(arr2, 0, m, mid);
 
-    }*/
+            if (up_cnt >= k) {
+                ans = Math.min(ans, mid);
+                right = mid - 1;
+            }
+            else
+                left = mid + 1;
+        }
+        return ans;
+    }
 }
